71ecce8ebfb74e4a0e79c6c2a04576d011e86abc
rohitmsd1819 <72183909+rohitmsd1819@users.noreply.github.com>
2020-10-01 23:33:35 +0530

    Add files via upload

---
+++ guessgame.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+using namespace std;
+void startgame();
+
+int main(){
+
+  int ch;
+  while(1){
+    cout<<"**********GUESSING GAME*************"<<endl;
+    cout<<"1. Start the Game"<<endl;
+    cout<<"2. Exit or End the Game."<<endl<<endl;
+
+    cout<<"ENTER YOUR CHOICE"<<endl;
+    cin>>ch;
+    switch(ch){
+      case 1:startgame();break;
+      case 2:exit(0);break;
+      default:cout<<"-----Invalid choice-------\n\n";
+    }
+  }
+  return 0;
+}
+
+void startgame(){
+    int secretNum=7;
+    int guess;
+    int guesscount=0;
+    cout<<"Get ready ....you have three chances to guess\n\n";
+
+    while(secretNum!=guess){
+        if(guesscount<3){
+            cout<<"Enter the Guess number:";
+            cin>>guess;
+            guesscount++;
+            if(secretNum==guess){
+                cout<<"\n-------YOU WON!!!!-------\n\n";break;
+            }
+        }
+        else{
+                cout<<"\n-------YOU LOSE!!!!-------\n\n";break;
+            }
+        }
+    }
+
